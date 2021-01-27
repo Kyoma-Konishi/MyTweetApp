f2330d8c9d484a3e71e923fd87967810a5409c6b
Kyoma-Konishi <kyo06ni18@gmail.com>
2021-01-27 21:30:46 +0900

    forth commit

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,13 +1,17 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!,except: [:index]
   def new
+    @tweet = Tweet.new
+  end
+  
+  def create
   end
 
   def index
+    @tweets = Tweet.all
   end
 
   def show
-  end
-
-  def create
+    @tweets = Tweets.find(params[:id])
   end
 end
